b0194883c6542d919ebff60f635632eb29e0f4d2
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-27 22:26:51 -0300

    New notifications thru pub/sub

--- app.simulator.js
+++ app.simulator.js
@@ -77,7 +77,7 @@
             for (var i = 0; i < 16; i++) {
                 this.evMatrix.push([]);
                 for (var j = 0; j < 32; j++) {
-                    this.evMatrix[i].push((((x * 32) + y) * 16 / 2731) | 0);
+                    this.evMatrix[i].push((((i * 32) + j) * 16 / 2731) | 0);
                 }
             }
             this.evNMatrix = [[false,
