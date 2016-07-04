b59980503a1e1e74ac178c1bc5c115577883f479
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-07-04 00:56:02 -0300

    Simpler implementation - operation chosen by client or stress tool

--- app.httpservice.js
+++ app.httpservice.js
@@ -18,13 +18,16 @@
 					}
 				})
 				.map(function(response) {
+					var h_reqId   = response.headers.get('X-ReqId') || response.headers.get('x-reqid'),
+					    h_rtt     = response.headers.get('X-Angular-Time') || response.headers.get('x-angular-time'),
+					    is_cached = (response.headers.get('X-Cache-Status') || response.headers.get('x-cache-status')) === 'HIT';
 					return {
 						"json":  response.json() || {},
-						"reqId": parseInt(response.headers.get('X-ReqId')||response.headers.get('x-reqid')),
-						"rtt":   parseInt(response.headers.get('X-Angular-Time') || response.headers.get('x-angular-time')),
-						"tsn":   (parseFloat(response.headers.get('X-nginX-Time') || response.headers.get('x-nginx-time')) * 1000.0) | 0,
-						"exts":  parseFloat(response.headers.get('X-Node-Time') || response.headers.get('x-node-time')),
-						"red":   parseFloat(response.headers.get('X-Redis-Time') || response.headers.get('x-redis-time'))
+						"reqId": parseInt(h_reqId),
+						"rtt":   parseInt(h_rtt),
+						"tsn":   is_cached ? 0 : (parseFloat(response.headers.get('X-nginX-Time') || response.headers.get('x-nginx-time')) * 1000.0) | 0,
+						"exts":  is_cached ? 0 : parseFloat(response.headers.get('X-Node-Time') || response.headers.get('x-node-time')),
+						"red":   is_cached ? 0 : parseFloat(response.headers.get('X-Redis-Time') || response.headers.get('x-redis-time'))
 					};
 				})
 				.catch();
