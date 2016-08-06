19a6743aa36cbba8dfa6e885aae240a7c8fd5fc2
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-08-06 19:52:07 -0300

    Improvements on PREFETCH and PRECONNECT and FontAwesome

--- ugly.sh
+++ ugly.sh
@@ -10,3 +10,31 @@ uglifyjs main.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,uns
 echo Minify ng2-nvd3.js
 uglifyjs ng2-nvd3.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o ng2-nvd3.js --stats
 echo Finished minifying files
+echo Gzipping static files
+gzip -k9 ./favicon.png
+gzip -k9 ./node_modules/bootstrap/dist/css/bootstrap.min.css
+gzip -k9 ./node_modules/nvd3/build/nv.d3.min.css
+gzip -k9 ./font-awesome-4.6.3/css/font-awesome.min.css
+gzip -k9 ./node_modules/jquery/dist/jquery.min.js
+gzip -k9 ./node_modules/bootstrap/dist/js/bootstrap.min.js
+gzip -k9 ./node_modules/d3/d3.min.js
+gzip -k9 ./node_modules/nvd3/build/nv.d3.min.js
+gzip -k9 ./node_modules/socket.io-client/socket.io.js
+gzip -k9 ./node_modules/intl/dist/Intl.min.js
+gzip -k9 ./node_modules/intl/locale-data/jsonp/en.js
+gzip -k9 ./node_modules/es6-shim/es6-shim.min.js
+gzip -k9 ./node_modules/zone.js/dist/zone.min.js
+gzip -k9 ./node_modules/reflect-metadata/Reflect.js
+gzip -k9 ./node_modules/rxjs/bundles/Rx.umd.min.js
+gzip -k9 ./node_modules/@angular/core/bundles/core.umd.min.js
+gzip -k9 ./node_modules/@angular/common/bundles/common.umd.min.js
+gzip -k9 ./node_modules/@angular/compiler/bundles/compiler.umd.min.js
+gzip -k9 ./node_modules/@angular/platform-browser/bundles/platform-browser.umd.min.js
+gzip -k9 ./node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js
+gzip -k9 ./http_profile.umd.js
+gzip -k9 ./ng2-nvd3.js
+gzip -k9 ./app.reference.js
+gzip -k9 ./app.httpservice.js
+gzip -k9 ./app.simulator.js
+gzip -k9 ./main.js
+echo Finished gzipping static files
