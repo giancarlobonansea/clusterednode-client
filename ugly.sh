25404294139e23e46405ec1bfd2543539ae70de1
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-09-04 12:45:03 -0300

    Upgrade to AngularJS 2.0.0-rc6

--- ugly.sh
+++ ugly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #echo Minify app.charts.service.js
 #uglifyjs app.charts.service.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.charts.service.js --source-map app.charts.service.js.map --source-map-include-sources
-#echo Minify app.googleanalytics.service.js
-#uglifyjs app.googleanalytics.service.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.googleanalytics.service.js --source-map app.googleanalytics.service.js.map --source-map-include-sources
+echo Minify app.googleanalytics.service.js
+uglifyjs app.googleanalytics.service.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.googleanalytics.service.js --source-map app.googleanalytics.service.js.map --source-map-include-sources
 #echo Minify app.httpg.service.js
 #uglifyjs app.httpg.service.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.httpg.service.js --source-map app.httpg.service.js.map --source-map-include-sources
 #echo Minify app.httpp.service.js
@@ -21,17 +21,17 @@
 #uglifyjs app.simulator.component.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.simulator.component.js --source-map app.simulator.component.js.map --source-map-include-sources
 #echo Minify app.stats.service.js
 #uglifyjs app.stats.service.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.stats.service.js --source-map app.stats.service.js.map --source-map-include-sources
-#echo Minify app.http.umd.library.js
-#uglifyjs app.http.umd.library.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.http.umd.library.js --source-map app.http.umd.library.js.map --source-map-include-sources
+echo Minify app.http.umd.library.js
+uglifyjs app.http.umd.library.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o app.http.umd.library.js --source-map app.http.umd.library.js.map --source-map-include-sources
 #echo Minify main.js
 #uglifyjs main.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m toplevel,eval -r '$,require,exports' -o main.js --source-map main.js.map --source-map-include-sources
 #echo Minify app.ng2nvd3.library.js
 #uglifyjs app.ng2nvd3.library.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o app.ng2nvd3.library.js --source-map app.ng2nvd3.library.js.map --source-map-include-sources
-#echo Minify Reflect.js
-#uglifyjs ./node_modules/reflect-metadata/Reflect.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o ./node_modules/reflect-metadata/Reflect.js
-#echo Minify socket.io.js
-#uglifyjs ./node_modules/socket.io-client/socket.io.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o ./node_modules/socket.io-client/socket.io.js
-#echo Finished minifying files
+echo Minify Reflect.js
+uglifyjs ./node_modules/reflect-metadata/Reflect.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o ./node_modules/reflect-metadata/Reflect.js
+echo Minify socket.io.js
+uglifyjs ./node_modules/socket.io-client/socket.io.js --screw-ie8 -c sequences,dead_code,conditionals,comparisons,unsafe_comps,evaluate,booleans,loops,unused,if_return,join_vars,collapse_vars,cascade,passes=5 -m -r '$,require,exports' -o ./node_modules/socket.io-client/socket.io.js
+echo Finished minifying files
 echo Gzipping static files
 gzip -k9f ./favicon.png
 gzip -k9f ./node_modules/bootstrap/dist/css/bootstrap.min.css
@@ -47,22 +47,22 @@ gzip -k9f ./node_modules/jquery/dist/jquery.min.js
 gzip -k9f ./node_modules/bootstrap/dist/js/bootstrap.min.js
 gzip -k9f ./node_modules/d3/d3.min.js
 gzip -k9f ./node_modules/nvd3/build/nv.d3.min.js
-#gzip -k9f ./node_modules/socket.io-client/socket.io.js
+gzip -k9f ./node_modules/socket.io-client/socket.io.js
 gzip -k9f ./node_modules/intl/dist/Intl.min.js
 gzip -k9f ./node_modules/intl/locale-data/jsonp/en.js
-#gzip -k9f ./node_modules/es6-shim/es6-shim.min.js
+gzip -k9f ./node_modules/es6-shim/es6-shim.min.js
 gzip -k9f ./node_modules/zone.js/dist/zone.min.js
-#gzip -k9f ./node_modules/reflect-metadata/Reflect.js
-#gzip -k9f ./node_modules/rxjs/bundles/Rx.umd.min.js
-#gzip -k9f ./node_modules/@angular/core/bundles/core.umd.min.js
-#gzip -k9f ./node_modules/@angular/common/bundles/common.umd.min.js
-#gzip -k9f ./node_modules/@angular/compiler/bundles/compiler.umd.min.js
-#gzip -k9f ./node_modules/@angular/platform-browser/bundles/platform-browser.umd.min.js
-#gzip -k9f ./node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js
-#gzip -k9f ./node_modules/@angular/forms/bundles/forms.umd.min.js
+gzip -k9f ./node_modules/reflect-metadata/Reflect.js
+gzip -k9f ./node_modules/rxjs/bundles/Rx.umd.min.js
+gzip -k9f ./node_modules/@angular/core/bundles/core.umd.min.js
+gzip -k9f ./node_modules/@angular/common/bundles/common.umd.min.js
+gzip -k9f ./node_modules/@angular/compiler/bundles/compiler.umd.min.js
+gzip -k9f ./node_modules/@angular/platform-browser/bundles/platform-browser.umd.min.js
+gzip -k9f ./node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js
+gzip -k9f ./node_modules/@angular/forms/bundles/forms.umd.min.js
 #gzip -k9f ./app.charts.service.js
-#gzip -k9f ./app.googleanalytics.service.js
-#gzip -k9f ./app.http.umd.library.js
+gzip -k9f ./app.googleanalytics.service.js
+gzip -k9f ./app.http.umd.library.js
 #gzip -k9f ./app.httpg.service.js
 #gzip -k9f ./app.httpp.service.js
 #gzip -k9f ./app.liveredis.service.js
